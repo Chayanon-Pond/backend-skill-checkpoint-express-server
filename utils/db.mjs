5557730386586512a06d85324f55f06fe959759a
Chayanon-Pond <chayanon.kmutnb@gmail.com>
2025-04-17 01:17:35 +0700

    feat(api): draft  implement POST /questions endpoint for creating new questions

--- utils/db.mjs
+++ utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:ppond5723@localhost:5432/Quora",
 });
 
 export default connectionPool;
